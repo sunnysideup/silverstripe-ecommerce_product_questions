1b4da1a20f18cd08c0fb66a03b62534066c71cfe
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-05-09 22:49:00 +0000

    ENHANCEMENT: check matches
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_questions/trunk@4296 950b781c-c544-efab-2f99-90083ee69683

--- code/model/ProductQuestion.php
+++ code/model/ProductQuestion.php
@@ -80,7 +80,8 @@ class ProductQuestion extends DataObject {
 	 * @return Array
 	 */
 	public static function create_file_array_from_option($option) {
-		$option = preg_replace("/[^a-zA-Z0-9_-]+/", '', $option);
+		$option = str_replace(' ', "-", $option);
+		$option = preg_replace("/[^a-z0-9_-]+/i", "", $option);
 		$imageOptions = array(
 			$option.".png",
 			$option.".PNG",

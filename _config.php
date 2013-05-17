bfb0dc73bc9d9553a25ea0d1094067089fc5d0d1
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-05-17 02:18:31 +0000

    ADDING variation options
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_questions/trunk@4326 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -1,9 +1,13 @@
 <?php
 
 // __________________________________ START ECOMMERCE PRODUCT QUESTIONS MODULE CONFIG __________________________________
-/**** MUST SET ****/
-//Object::add_extension("Product_Controller", "ProductQuestion_ProductDecorator");
+//**** MUST SET
+//Object::add_extension("Product", "ProductQuestion_ProductDecorator");
+//Object::add_extension("ProductPage_Controller", "ProductQuestion_ProductControllerDecorator");
 //Object::add_extension("OrderItem", "ProductQuestion_OrderItemExtension");
+//**** MAY SET
+//Object::add_extension("ProductVariation", "ProductQuestion_ProductVariationDecorator");
+
 
 /***** HIGHLY RECOMMENDED ****
 ecommerce.yaml:

6073ac79ac68a3e9ba2f9295b7b3eb74acf387e4
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:13:02 +1300

    Converted to PSR-2

--- code/model/ProductQuestion_ProductControllerDecorator.php
+++ code/model/ProductQuestion_ProductControllerDecorator.php
@@ -8,110 +8,114 @@
  *
  *
  */
-class ProductQuestion_ProductControllerDecorator extends Extension {
+class ProductQuestion_ProductControllerDecorator extends Extension
+{
 
 
-	/**
-	 * we need this here to
-	 * because otherwise the extension will not work
-	 */
-	private static $allowed_actions = array(
-		"ProductQuestionsAnswerForm",
-		"productquestionsanswerselect"
-	);
+    /**
+     * we need this here to
+     * because otherwise the extension will not work
+     */
+    private static $allowed_actions = array(
+        "ProductQuestionsAnswerForm",
+        "productquestionsanswerselect"
+    );
 
-	/**
-	 * Stores the related OrderItem
-	 * @var OrderItem
-	 */
-	protected $productQuestionOrderItem = null;
+    /**
+     * Stores the related OrderItem
+     * @var OrderItem
+     */
+    protected $productQuestionOrderItem = null;
 
-	/**
-	 * renders a form with the product questions
-	 * @return String (HTML)
-	 */
-	function productquestionsanswerselect(){
-		$this->getProductQuestionOrderItem();
-		return $this->owner->customise(
-			array(
-				"Title" => $this->productQuestionOrderItem->getTableTitle(),
-				"Form" => $this->ProductQuestionsAnswerForm()
-			)
-		)->renderWith("productquestionsanswerselect") ;
-	}
+    /**
+     * renders a form with the product questions
+     * @return String (HTML)
+     */
+    public function productquestionsanswerselect()
+    {
+        $this->getProductQuestionOrderItem();
+        return $this->owner->customise(
+            array(
+                "Title" => $this->productQuestionOrderItem->getTableTitle(),
+                "Form" => $this->ProductQuestionsAnswerForm()
+            )
+        )->renderWith("productquestionsanswerselect") ;
+    }
 
-	/**
-	 * returns a form with questions
-	 * @return Form
-	 */
-	function ProductQuestionsAnswerForm(){
-		$this->getProductQuestionOrderItem();
-		if($this->productQuestionOrderItem) {
-			return $this->productQuestionOrderItem->ProductQuestionsAnswerForm($this->owner, $name = "ProductQuestionsAnswerForm");
-		}
-	}
+    /**
+     * returns a form with questions
+     * @return Form
+     */
+    public function ProductQuestionsAnswerForm()
+    {
+        $this->getProductQuestionOrderItem();
+        if ($this->productQuestionOrderItem) {
+            return $this->productQuestionOrderItem->ProductQuestionsAnswerForm($this->owner, $name = "ProductQuestionsAnswerForm");
+        }
+    }
 
-	/**
-	 * returns the fields from the form
-	 * @return FieldSet
-	 */
-	function ProductQuestionsAnswerFormFields(){
-		$fieldSet = new FieldList();
-		$product = $this->owner->dataRecord;
-		$productQuestions = $product->ApplicableProductQuestions();
-		if($productQuestions) {
-			foreach($productQuestions as $productQuestion) {
-				$fieldSet->push($productQuestion->getFieldForProduct($product));
-			}
-		}
-		return $fieldSet;
-	}
+    /**
+     * returns the fields from the form
+     * @return FieldSet
+     */
+    public function ProductQuestionsAnswerFormFields()
+    {
+        $fieldSet = new FieldList();
+        $product = $this->owner->dataRecord;
+        $productQuestions = $product->ApplicableProductQuestions();
+        if ($productQuestions) {
+            foreach ($productQuestions as $productQuestion) {
+                $fieldSet->push($productQuestion->getFieldForProduct($product));
+            }
+        }
+        return $fieldSet;
+    }
 
-	/**
-	 * processes a form and
-	 * adds product question answer(s) to order item.
-	 * The answers are added as HTML and JSON
-	 * and redirects back to the previous page or a set BackURL
-	 * (set in the form data)
-	 * @param Array $data - form data
-	 * @param Form form - data from the form
-	 */
-	function addproductquestionsanswer($data, $form){
-		$this->getProductQuestionOrderItem();
-		$data = Convert::raw2sql($data);
-		if($this->productQuestionOrderItem) {
-			$this->productQuestionOrderItem->updateOrderItemWithProductAnswers(
-				$answers = $data["ProductQuestions"],
-				$write = true
-			);
-		}
-		if(isset($data["BackURL"])){
-			$this->owner->redirect($data["BackURL"]);
-		}
-		else {
-			$this->owner->redirectBack();
-		}
-	}
+    /**
+     * processes a form and
+     * adds product question answer(s) to order item.
+     * The answers are added as HTML and JSON
+     * and redirects back to the previous page or a set BackURL
+     * (set in the form data)
+     * @param Array $data - form data
+     * @param Form form - data from the form
+     */
+    public function addproductquestionsanswer($data, $form)
+    {
+        $this->getProductQuestionOrderItem();
+        $data = Convert::raw2sql($data);
+        if ($this->productQuestionOrderItem) {
+            $this->productQuestionOrderItem->updateOrderItemWithProductAnswers(
+                $answers = $data["ProductQuestions"],
+                $write = true
+            );
+        }
+        if (isset($data["BackURL"])) {
+            $this->owner->redirect($data["BackURL"]);
+        } else {
+            $this->owner->redirectBack();
+        }
+    }
 
-	/**
-	 * retrieves order item from post / get variables.
-	 * @return OrderItem | Null
-	 */
-	protected function getProductQuestionOrderItem(){
-		$id = intval($this->owner->request->param("ID"));
-		if(!$id) {
-			$id = intval($this->owner->request->postVar("OrderItemID"));
-		}
-		if(!$id) {
-			$id = intval($this->owner->request->getVar("OrderItemID"));
-		}
-		if($id) {
-			$this->productQuestionOrderItem = OrderItem::get()->byID($id);
-		}
-		if(!$this->productQuestionOrderItem) {
-			user_error("NO this->productQuestionOrderItem specified");
-		}
-		return $this->productQuestionOrderItem;
-	}
-
+    /**
+     * retrieves order item from post / get variables.
+     * @return OrderItem | Null
+     */
+    protected function getProductQuestionOrderItem()
+    {
+        $id = intval($this->owner->request->param("ID"));
+        if (!$id) {
+            $id = intval($this->owner->request->postVar("OrderItemID"));
+        }
+        if (!$id) {
+            $id = intval($this->owner->request->getVar("OrderItemID"));
+        }
+        if ($id) {
+            $this->productQuestionOrderItem = OrderItem::get()->byID($id);
+        }
+        if (!$this->productQuestionOrderItem) {
+            user_error("NO this->productQuestionOrderItem specified");
+        }
+        return $this->productQuestionOrderItem;
+    }
 }
--- code/model/ProductQuestion_ProductVariationExtension.php
+++ code/model/ProductQuestion_ProductVariationExtension.php
@@ -8,146 +8,150 @@
  *
  * @author nicolaas <modules@sunnysideup.co.nz>
  */
-class ProductQuestion_ProductVariationDecorator extends DataExtension {
+class ProductQuestion_ProductVariationDecorator extends DataExtension
+{
 
-	private static $db = array("ConfigureLabel" => 'Varchar(50)');
+    private static $db = array("ConfigureLabel" => 'Varchar(50)');
 
-	private static $many_many = array(
-		"IgnoreProductQuestions" => 'ProductQuestion',
-		"AdditionalProductQuestions" => 'ProductQuestion'
-	);
+    private static $many_many = array(
+        "IgnoreProductQuestions" => 'ProductQuestion',
+        "AdditionalProductQuestions" => 'ProductQuestion'
+    );
 
-	function updateCMSFields(FieldList $fields) {
-		parent::updateCMSFields($fields);
-		if(ProductQuestion::get()->count()) {
-			$productQuestionsDefault = $this->owner->Product()->ApplicableProductQuestions();
-			$productQuestionsDefaultArray = array(0 => 0);
-			if($productQuestionsDefault && $productQuestionsDefault->count()){
-				$productQuestionsDefaultArray = $productQuestionsDefault->map("ID", "FullName")->toArray();
-				$fields->addFieldToTab(
-					"Root.Questions",
-					new CheckboxSetField("IgnoreProductQuestions",
-						_t("ProductQuestions.IGNORE_QUESTIONS", "Ignore Questions for this variation"),
-						$productQuestionsDefaultArray
-					)
-				);
-			}
-			$productQuestionsAdditional = ProductQuestion::get()->exclude(array("ID" => array_flip($productQuestionsDefaultArray)));
-			if($productQuestionsAdditional->count()){
-				$productQuestionsAdditionalArray = $productQuestionsAdditional->map("ID", "FullName")->toArray();
-				$fields->addFieldToTab(
-					"Root.Questions",
-					new CheckboxSetField(
-						"AdditionalProductQuestions",
-						_t("ProductQuestions.ADDITIONAL_QUESTIONS", "Additional Questions for this variation"),
-						$productQuestionsAdditionalArray
-					)
-				);
-			}
-		}
-	}
+    public function updateCMSFields(FieldList $fields)
+    {
+        parent::updateCMSFields($fields);
+        if (ProductQuestion::get()->count()) {
+            $productQuestionsDefault = $this->owner->Product()->ApplicableProductQuestions();
+            $productQuestionsDefaultArray = array(0 => 0);
+            if ($productQuestionsDefault && $productQuestionsDefault->count()) {
+                $productQuestionsDefaultArray = $productQuestionsDefault->map("ID", "FullName")->toArray();
+                $fields->addFieldToTab(
+                    "Root.Questions",
+                    new CheckboxSetField("IgnoreProductQuestions",
+                        _t("ProductQuestions.IGNORE_QUESTIONS", "Ignore Questions for this variation"),
+                        $productQuestionsDefaultArray
+                    )
+                );
+            }
+            $productQuestionsAdditional = ProductQuestion::get()->exclude(array("ID" => array_flip($productQuestionsDefaultArray)));
+            if ($productQuestionsAdditional->count()) {
+                $productQuestionsAdditionalArray = $productQuestionsAdditional->map("ID", "FullName")->toArray();
+                $fields->addFieldToTab(
+                    "Root.Questions",
+                    new CheckboxSetField(
+                        "AdditionalProductQuestions",
+                        _t("ProductQuestions.ADDITIONAL_QUESTIONS", "Additional Questions for this variation"),
+                        $productQuestionsAdditionalArray
+                    )
+                );
+            }
+        }
+    }
 
-	/**
-	 * returns the fields from the form
-	 * @return FieldSet
-	 */
-	function ProductQuestionsAnswerFormFields(){
-		$fieldSet = new FieldList();
-		$productQuestions = $this->owner->ApplicableProductQuestions();
-		if($productQuestions && $productQuestions->count()) {
-			foreach($productQuestions as $productQuestion) {
-				$fieldSet->push($productQuestion->getFieldForProduct($this));
-			}
-		}
-		return $fieldSet;
-	}
+    /**
+     * returns the fields from the form
+     * @return FieldSet
+     */
+    public function ProductQuestionsAnswerFormFields()
+    {
+        $fieldSet = new FieldList();
+        $productQuestions = $this->owner->ApplicableProductQuestions();
+        if ($productQuestions && $productQuestions->count()) {
+            foreach ($productQuestions as $productQuestion) {
+                $fieldSet->push($productQuestion->getFieldForProduct($this));
+            }
+        }
+        return $fieldSet;
+    }
 
-	/**
-	 * returns a label that is used to allow customers to open the form
-	 * for answering the Product Questions.
-	 * @return String
-	 */
-	public function CustomConfigureLabel(){
-		if($this->owner->HasProductQuestions()) {
-			if($this->owner->ConfigureLabel) {
-				return $this->owner->ConfigureLabel;
-			}
-			elseif($product = $this->owner->Product()) {
-				if($label = $product->owner->CustomConfigureLabel()) {
-					return $label;
-				}
-			}
-		}
-		return "";
-	}
+    /**
+     * returns a label that is used to allow customers to open the form
+     * for answering the Product Questions.
+     * @return String
+     */
+    public function CustomConfigureLabel()
+    {
+        if ($this->owner->HasProductQuestions()) {
+            if ($this->owner->ConfigureLabel) {
+                return $this->owner->ConfigureLabel;
+            } elseif ($product = $this->owner->Product()) {
+                if ($label = $product->owner->CustomConfigureLabel()) {
+                    return $label;
+                }
+            }
+        }
+        return "";
+    }
 
-	/**
-	 *
-	 * @return String
-	 */
-	function ProductQuestionsAnswerFormLink($id = 0){
-		return $this->owner->Link("productquestionsanswerselect")."/".$id."/?BackURL=".urlencode(Controller::curr()->Link());
-	}
+    /**
+     *
+     * @return String
+     */
+    public function ProductQuestionsAnswerFormLink($id = 0)
+    {
+        return $this->owner->Link("productquestionsanswerselect")."/".$id."/?BackURL=".urlencode(Controller::curr()->Link());
+    }
 
-	/**
-	 * saves the list of product questions
-	 * @var NULL | DataList
-	 */
-	private static $_product_questions_cache = array();
+    /**
+     * saves the list of product questions
+     * @var NULL | DataList
+     */
+    private static $_product_questions_cache = array();
 
-	/**
-	 *
-	 * @alais for ProductQuestions
-	 */ 
-	function ApplicableProductQuestions() {
-		return $this->ProductQuestions();
-	}
+    /**
+     *
+     * @alais for ProductQuestions
+     */
+    public function ApplicableProductQuestions()
+    {
+        return $this->ProductQuestions();
+    }
 
-	/**
-	 * returns the applicable Product Questions
-	 * @return DataList
-	 */
-	function ProductQuestions(){
-		if(!isset(self::$_product_questions_cache[$this->owner->ID])) {
-			$product = $this->owner->Product();
-			$productQuestions = $product->ApplicableProductQuestions();
-			$productQuestionsArray = array(0 => 0);
-			if($productQuestions && $productQuestions->count()) {
-				$productQuestionsArray = $productQuestions->map("ID", "ID")->toArray();
-			}
-			$ignoreProductQuestions = $this->owner->IgnoreProductQuestions();
-			if($ignoreProductQuestions && $ignoreProductQuestions->count()) {
-				foreach($ignoreProductQuestions as $ignoreProductQuestion) {
-					unset($productQuestionsArray[$ignoreProductQuestion->ID]);
-				}
-			}
-			$additionalProductQuestions = $this->owner->AdditionalProductQuestions();
-			if($additionalProductQuestions && $additionalProductQuestions->count()) {
-				foreach($additionalProductQuestions as $additionalProductQuestion) {
-					$productQuestionsArray[$additionalProductQuestion->ID] = $additionalProductQuestion->ID;
-				}
-			}
-			if(!count($productQuestionsArray)) {
-				$productQuestionsArray = array(0 => 0);
-			}
-			self::$_product_questions_cache[$this->owner->ID] = ProductQuestion::get()->filter(array("ID" => $productQuestionsArray));
-		}
-		return self::$_product_questions_cache[$this->owner->ID];
-	}
+    /**
+     * returns the applicable Product Questions
+     * @return DataList
+     */
+    public function ProductQuestions()
+    {
+        if (!isset(self::$_product_questions_cache[$this->owner->ID])) {
+            $product = $this->owner->Product();
+            $productQuestions = $product->ApplicableProductQuestions();
+            $productQuestionsArray = array(0 => 0);
+            if ($productQuestions && $productQuestions->count()) {
+                $productQuestionsArray = $productQuestions->map("ID", "ID")->toArray();
+            }
+            $ignoreProductQuestions = $this->owner->IgnoreProductQuestions();
+            if ($ignoreProductQuestions && $ignoreProductQuestions->count()) {
+                foreach ($ignoreProductQuestions as $ignoreProductQuestion) {
+                    unset($productQuestionsArray[$ignoreProductQuestion->ID]);
+                }
+            }
+            $additionalProductQuestions = $this->owner->AdditionalProductQuestions();
+            if ($additionalProductQuestions && $additionalProductQuestions->count()) {
+                foreach ($additionalProductQuestions as $additionalProductQuestion) {
+                    $productQuestionsArray[$additionalProductQuestion->ID] = $additionalProductQuestion->ID;
+                }
+            }
+            if (!count($productQuestionsArray)) {
+                $productQuestionsArray = array(0 => 0);
+            }
+            self::$_product_questions_cache[$this->owner->ID] = ProductQuestion::get()->filter(array("ID" => $productQuestionsArray));
+        }
+        return self::$_product_questions_cache[$this->owner->ID];
+    }
 
-	/**
-	 * Does this buyable have product questions?
-	 * @return Boolean
-	 */
-	public function HasProductQuestions(){
-		if($applicable = $this->owner->ApplicableProductQuestions()) {
-			if($applicable->count()) {
-				return true;
-			}
-		}
-		return false;
-	}
-
+    /**
+     * Does this buyable have product questions?
+     * @return Boolean
+     */
+    public function HasProductQuestions()
+    {
+        if ($applicable = $this->owner->ApplicableProductQuestions()) {
+            if ($applicable->count()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
-
-

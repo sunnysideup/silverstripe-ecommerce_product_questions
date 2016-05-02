5cc969d8fb9a9fb95ba14b44b29f5e18f1b17c21
Sunny Side Up <github@sunnysideup.co.nz>
2016-05-02 20:14:03 +1200

    minor

--- code/model/ProductQuestion_OrderExtension.php
+++ code/model/ProductQuestion_OrderExtension.php
@@ -9,14 +9,18 @@
 
 class ProductQuestion_OrderExtension extends DataExtension {
 
-        function updateSubmitErrors(){
-                $array = array();
-                foreach($this->owner->OrderItems() as $item) {
-                        if(!$item->AllRequiredQuestionsAnswered()) {
-                                $txt = _t("ProductQuestion.PROVIDE_MORE_INFORMATION", "Provide more information for");
-                                $array[$item->ID] = $txt." <em>".$item->getTableTitle()."</em>";
-                        }
-                }
-                return count($array) ? $array : null;
+    /**
+     *
+     * @return null | array
+     */
+    function updateSubmitErrors(){
+        $array = array();
+        foreach($this->owner->OrderItems() as $item) {
+            if(!$item->AllRequiredQuestionsAnswered()) {
+                $txt = _t("ProductQuestion.PROVIDE_MORE_INFORMATION", "Please answer question in relation to:");
+                $array[$item->ID] = $txt." <em>".$item->getTableTitle()."</em>";
+            }
         }
+        return count($array) ? $array : null;
+    }
 }
